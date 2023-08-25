bd61bbeb91a5dbe7af68ad475fec90e0b55cbd73
Iris <bensassin6@gmail.com>
2023-08-25 09:52:02 +0100

    updated

--- Desktop/generate-password/public/js/app.js
+++ Desktop/generate-password/public/js/app.js
@@ -1,75 +1,83 @@
-const characters = [
-    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
-    "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F",
-    "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
-    "W", "X", "Y", "Z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "!", "?",
-    "{", "}", "-", "+", "=", "[", "]", "|", "~", "`", "@", "#", " "
-];
+// Define the characters array as a string instead of an array
+const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!?{}-+=[]|~`@#_/*";
 
-let passElOne = document.querySelector("#pass-el-one");
-let passElTwo = document.querySelector("#pass-el-two");
-let passElThree = document.querySelector("#pass-el-three");
-let passElFour = document.querySelector("#pass-el-four");
-let generateBtn = document.querySelector("#generate")
-let lengthValue = document.querySelector("#length-value")
+// Query the elements only once using a single querySelectorAll call
+const passwordContainers = document.querySelectorAll(".password");
+const generateBtn = document.querySelector("#generate");
+const lengthValue = document.querySelector("#length-value");
+const strengthIndicator = document.querySelector(".strength-indicator");
+const savedPasswordsList = document.querySelector("#savedPasswordsList");
+const backupCodesList = document.querySelector("#backupCodesList");
+const generateBackupCodesBtn = document.querySelector("#generateBackupCodes");
+const restoreBackupCodesBtn = document.querySelector("#restoreBackupCodes");
+const restorePasswordsBtn = document.querySelector("#restorePasswordsBtn");
 
-//get random letter from characters array
+// Get a random character directly from the string
 function randomCharacter() {
-    let randomNumber = Math.floor(Math.random() * characters.length);
+    const randomNumber = Math.floor(Math.random() * characters.length);
     return characters[randomNumber];
 }
 
-//ability to set password length
+// Set a default password length
 function setPassLength() {
-    let inputValue = lengthValue.value;
-    console.log(inputValue);
-    
-    //setting default
-    if (inputValue === ""){
-        return 12;
-    }
-    return inputValue;
+    const inputValue = lengthValue.value;
+    return inputValue === "" ? 12 : inputValue;
 }
 
-//generating the password
+// Generate a password of the given length
 function generatePassword(length) {
     let password = '';
-    for (let i = 0; i < length; i++){
+    for (let i = 0; i < length; i++) {
         password += randomCharacter();
     }
     return password;
 }
 
-// rendering password to screen
-function renderPassword() {
-    passElOne.textContent = '';
-    passElTwo.textContent = '';
-    passElThree.textContent = '';
-    passElFour.textContent = '';
-    
-    let length = setPassLength()
-    passElOne.textContent += generatePassword(length);
-    passElTwo.textContent += generatePassword(length);
-    passElThree.textContent += generatePassword(length);
-    passElFour.textContent += generatePassword(length);
+// Calculate password strength based on length
+function calculatePasswordStrength(password) {
+    return Math.min(100, (password.length / 20) * 100); 
 }
 
-// passElOne.addEventListener('click', function(e){
-//     console.log(e.target.textContent)
-// })
+// Update the strength indicator color and width
+function updateStrengthIndicator(password) {
+    const strength = calculatePasswordStrength(password);
+    strengthIndicator.style.width = `${strength}%`;
 
-//ability to copy to clipboard
-function copyToClipboard(e) {
-    let password = e.target;
-    navigator.clipboard.writeText(password.textContent).then(res=>{
-            console.log("Input data copied to clipboard successfully");
-        })
-    
+    if (strength < 50) {
+        strengthIndicator.style.backgroundColor = "red";
+    } else if (strength < 80) {
+        strengthIndicator.style.backgroundColor = "orange";
+    } else {
+        strengthIndicator.style.backgroundColor = "green";
+    }
 }
 
-passElOne.addEventListener("click", copyToClipboard)
-passElTwo.addEventListener("click", copyToClipboard)
-passElThree.addEventListener("click", copyToClipboard)
-passElFour.addEventListener("click", copyToClipboard)
+// Render generated passwords to the screen
+function renderPasswords() {
+    passwordContainers.forEach(container => {
+        const length = setPassLength();
+        const password = generatePassword(length);
+        container.textContent = password;
+        updateStrengthIndicator(password);
+    });
+}
 
-generateBtn.addEventListener("click", renderPassword)
+// Copy password to clipboard
+function copyToClipboard(password) {
+    navigator.clipboard.writeText(password).then(() => {
+        console.log("Password copied to clipboard successfully");
+    });
+}
+
+// Attach event listeners
+passwordContainers.forEach(container => {
+    container.addEventListener("click", () => {
+        copyToClipboard(container.textContent);
+        savePassword(container.textContent); // Save the password
+    });
+});
+
+generateBtn.addEventListener("click", renderPasswords);
+
+const savedPasswords = []; // Simulated saved passwords array
+
